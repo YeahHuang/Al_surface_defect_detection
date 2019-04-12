f8dbd24e9c653e59983ae3e230886c233edc186e
YeahHuang <534852491@qq.com>
2019-04-12 11:08:11 +0800

    Update test.sh
    Fixed the original bug after ECNUJason's kind reminder

--- Season2-localization/code1/test.sh
+++ Season2-localization/code1/test.sh
@@ -1,2 +1,2 @@
-python tools/infer1.py --cfg configs/defects/defect_test_aug.yaml --output-dir detectron-result --image-ext jpg --thresh 0.2 --wts  detectron-output/train/defects_train/generalized_rcnn/model_iter79999.pkl testb
+python tools/infer1.py --cfg test.yaml --output-dir detectron-result --image-ext jpg --thresh 0.2 --wts  detectron-output/train/defects_train/generalized_rcnn/model_iter79999.pkl testb
 
